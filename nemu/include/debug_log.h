aa7287427b1ac530365a20aff9f5ec3b055ebf48
lbh <1370656133@qq.com>
2022-11-22 14:59:01 +0800

    Add new debug log method.

---
+++ nemu/include/debug_log.h
@@ -0,0 +1,130 @@
+
+#ifndef __DEBUG_LOG_H__
+#define __DEBUG_LOG_H__
+
+#include <common.h>
+#include <stdio.h>
+#include <utils.h>
+
+/*
+ * The macro definitions for debug
+ *
+ * These macros are defined in static. If you want to use debug macro, you can
+ * use as following code:
+ *
+ * In your C/C++ file, enable/disable DEBUG_ENABLE macro, and then include this
+ * header file.
+ *
+ * #define DBG_TAG           "MOD_TAG"
+ * #define DBG_LVL           DBG_INFO
+ * #include <debug_log.h>          // must after of DBG_LVL, DBG_TAG or other options
+ *
+ * Then in your C/C++ file, you can use LOG_X macro to print out logs:
+ * LOG_D("this is a debug log!");
+ * LOG_E("this is a error log!");
+ */
+
+#define DBG_ENABLE
+#define DBG_COLOR
+
+/* DEBUG level */
+#define DBG_ERROR           0
+#define DBG_WARNING         1
+#define DBG_INFO            2
+#define DBG_LOG             3
+
+#ifdef DBG_TAG
+#ifndef DBG_SECTION_NAME
+#define DBG_SECTION_NAME    DBG_TAG
+#endif
+#else
+/* compatible with old version */
+#ifndef DBG_SECTION_NAME
+#define DBG_SECTION_NAME    "DBG"
+#endif
+#endif /* DBG_TAG */
+
+#ifdef DBG_ENABLE
+
+#ifdef DBG_LVL
+#ifndef DBG_LEVEL
+#define DBG_LEVEL         DBG_LVL
+#endif
+#else
+/* compatible with old version */
+#ifndef DBG_LEVEL
+#define DBG_LEVEL         DBG_WARNING
+#endif
+#endif /* DBG_LVL */
+/*
+ * The color for terminal (foreground)
+ * BLACK    30
+ * RED      31
+ * GREEN    32
+ * YELLOW   33
+ * BLUE     34
+ * PURPLE   35
+ * CYAN     36
+ * WHITE    37
+ */
+#ifdef DBG_COLOR
+#define _DBG_COLOR(n)        rt_kprintf("\033["#n"m")
+#define _DBG_LOG_HDR(lvl_name, color_n)                    \
+    printf("\033["#color_n"m[" lvl_name "/" DBG_SECTION_NAME "] ")
+#define _DBG_LOG_X_END                                     \
+    printf("\033[0m\n")
+#else
+#define _DBG_COLOR(n)
+#define _DBG_LOG_HDR(lvl_name, color_n)                    \
+    printf("[" lvl_name "/" DBG_SECTION_NAME "] ")
+#define _DBG_LOG_X_END                                     \
+    printf("\n")
+#endif /* DBG_COLOR */
+
+#define dbg_log_line(lvl, color_n, fmt, ...)                \
+    do                                                      \
+    {                                                       \
+        _DBG_LOG_HDR(lvl, color_n);                         \
+        printf(fmt, ##__VA_ARGS__);                     \
+        _DBG_LOG_X_END;                                     \
+    }                                                       \
+    while (0)
+#define dbg_raw(...)         printf(__VA_ARGS__);
+
+#else
+#define dbg_log(level, fmt, ...)
+#define dbg_here
+#define dbg_enter
+#define dbg_exit
+#define dbg_log_line(lvl, color_n, fmt, ...)
+#define dbg_raw(...)
+#endif /* DBG_ENABLE */
+
+
+#if (DBG_LEVEL >= DBG_LOG)
+#define LOG_D(fmt, ...)      dbg_log_line("D", 36, fmt, ##__VA_ARGS__)
+#else
+#define LOG_D(...)
+```
+#endif
+
+#if (DBG_LEVEL >= DBG_INFO)
+#define LOG_I(fmt, ...)      dbg_log_line("I", 32, fmt, ##__VA_ARGS__)
+#else
+#define LOG_I(...)
+#endif
+
+#if (DBG_LEVEL >= DBG_WARNING)
+#define LOG_W(fmt, ...)      dbg_log_line("W", 33, fmt, ##__VA_ARGS__)
+#else
+#define LOG_W(...)
+#endif
+
+#if (DBG_LEVEL >= DBG_ERROR)
+#define LOG_E(fmt, ...)      dbg_log_line("E", 31, fmt, ##__VA_ARGS__)
+#else
+#define LOG_E(...)
+#endif
+
+
+#endif /* __DEBUG_LOG_H__ */
