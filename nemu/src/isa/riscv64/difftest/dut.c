4d0e13e07017ed651a69aba379b4c4542e521543
lbh <1370656133@qq.com>
2022-12-23 11:29:55 +0800

    Enabled differential testing.

--- nemu/src/isa/riscv64/difftest/dut.c
+++ nemu/src/isa/riscv64/difftest/dut.c
@@ -18,7 +18,18 @@
 #include "../local-include/reg.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  uint32_t i;
+  bool is_the_same = true;
+  for(i=0; i<(sizeof(cpu.gpr)/sizeof(cpu.gpr[0])); i++)
+  {
+      if (ref_r->gpr[i] != cpu.gpr[i])
+      {
+          printf("difftest: ref pc : 0x%8lx, reg number = %d, ref is : 0x%8lx, DUT is 0x%8lx \r\n" ,pc, i, ref_r->gpr[i], cpu.gpr[i]);
+          is_the_same = false;
+      }
+
+  }
+  return is_the_same;
 }
 
 void isa_difftest_attach() {
