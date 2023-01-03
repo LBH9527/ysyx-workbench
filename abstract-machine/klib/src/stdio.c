564e51c7aa5b5caac84c7eb586e253aab521ab85
lbh <1370656133@qq.com>
2023-01-03 16:59:41 +0800

    update stdio and string of klib .

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,24 +5,147 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+#define PRINTF_BUF_LENGTH  128
+
+int printf(const char *fmt, ...) 
+{
+  // panic("Not implemented");
+  int ret = 0;
+  char pBuf[PRINTF_BUF_LENGTH];
+
+  va_list ap;
+  va_start(ap, fmt);
+  ret = vsprintf(pBuf, fmt, ap);
+  va_end(ap);
+
+  putstr(pBuf);
+
+  return ret;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsprintf(char *out, const char *fmt, va_list ap) 
+{
+    return (vsnprintf(out, sizeof(out), fmt, ap));
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  int ret = 0;
+   va_list ap;
+
+  va_start(ap, fmt);
+  ret = vsprintf(out, fmt, ap);
+  va_end(ap);
+
+  return ret;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  int ret = 0;
+  va_list ap;
+
+  va_start(ap, fmt);
+  vsnprintf(out, n, fmt, ap);
+  va_end(ap);
+
+  return ret;
+}
+
+static int uint64_to_str(uint64_t ui64Val, char *pcBuf)
+{
+  uint32_t i;
+  uint32_t length ;
+  char *psrc;
+  int ret = 0;
+ 
+  psrc = pcBuf;
+  i = 1;
+  length = 0;
+  while(ui64Val / i )
+  {
+    length++;
+    i = i*10;
+  }
+  ret = length;
+
+  while(length)
+  {
+    length--;
+    *(psrc + length)  = (char)(ui64Val % 10 + 0x30);   //0x30 : digital to ascii
+    ui64Val = ui64Val / 10;    
+  }
+
+  return ret;
 }
 
 int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  // uint32_t i;
+  // uint32_t flag;
+  const char *src;
+  char *dst;
+  int d;
+  char *s;
+  // char c;
+  int temp;
+  int char_count = 0;
+
+  src = fmt;
+  dst = out;
+
+  while (*src)
+  {
+    if (*src == '%')
+    { 
+      src++;
+      switch (*src) 
+      {
+        case 's':              /* string */
+            s = va_arg(ap, char *);
+            while(*s != '\0')
+            {
+              *dst = *s;
+              dst ++;
+              s ++;
+              char_count++;
+            }  
+            break;
+        case 'd':              /* int */
+            d = va_arg(ap, int);
+            temp = uint64_to_str(d, dst);
+            dst += temp;           
+            char_count += temp; 
+            break;
+        // case 'c':              /* char */
+        //     /* need a cast here since va_arg only
+        //       takes fully promoted types */
+        //     c = (char) va_arg(ap, int);
+        //     break;
+        default:
+            assert(0);
+            break;
+      }
+      src++;
+    }
+    else
+    {
+      *dst = *src;
+      src++;
+      dst++;
+      char_count++;
+    }
+  }
+
+  //
+  // Terminate the string
+  //
+  if ( dst )
+  {
+      *dst = '\0';
+  }
+
+  return char_count;
 }
 
 #endif
--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -4,44 +4,207 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+// strlen is with out null character
 size_t strlen(const char *s) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(s != NULL);
+  const char *ps = s;
+  size_t len = 0;
+
+  while(*ps != '\0')
+  {
+    ps++;
+    len++;
+  }
+
+  return len;
 }
 
 char *strcpy(char *dst, const char *src) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(dst != NULL);
+  assert(src != NULL);
+  assert(sizeof(dst) >= sizeof(src));
+
+  char *pdst = dst;
+  const char *psrc = src;
+
+  while(*psrc != '\0')
+  {
+    *pdst = *psrc;
+    pdst++;
+    psrc++;
+  }
+
+  return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(dst != NULL);
+  assert(src != NULL);
+  assert(sizeof(dst) >= n);
+
+  char *pdst = dst;
+  const char *psrc = src;
+  while(n--)
+  {
+    *pdst = *psrc;
+    pdst++;
+    psrc++;
+  }
+
+  return dst;
 }
 
 char *strcat(char *dst, const char *src) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(dst != NULL);
+  assert(src != NULL);
+  assert(sizeof(dst) > (strlen(dst) + strlen(src)) );
+  char *pdst = dst;
+  const char *psrc = src;
+ 
+  while(*pdst != '\0')
+  {
+    pdst++;
+  }
+
+  while(*psrc != '\0')
+  {
+    *pdst = *psrc;  
+    pdst++;
+    psrc++;
+  }   
+
+  return dst;
 }
 
+//The  strcmp()  function compares the two strings s1 and s2.  
+// The locale is not taken into account (for a locale-aware comparison, see strcoll(3)).  
+// It returns an integer less than, equal to, or greater than zero if s1 is found, respectively, 
+// to be less than, to match, or be greater than s2.
 int strcmp(const char *s1, const char *s2) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(s1 != NULL && s2 != NULL);
+  const char *ps1 = s1;
+  const char *ps2 = s2;
+
+  while(*ps1 != '\0')
+  {
+    if(*ps1 == *ps2)
+    {
+      ps1++;
+      ps2++;
+    }
+    else
+    {
+      break;
+    }
+  }
+  
+  return (*ps1 - *ps2);
+
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(s1 != NULL && s2 != NULL);
+  const char *ps1 = s1;
+  const char *ps2 = s2;
+
+  while(n--)
+  {
+    if(*ps1 == *ps2)
+    {
+      ps1++;
+      ps2++;
+    }
+    else
+    {
+      break;
+    }
+  }
+  
+  return (*ps1 - *ps2);
 }
 
 void *memset(void *s, int c, size_t n) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(s != NULL);
+  // printf("sizeof s is %d \r\n", sizeof(s));
+  assert(sizeof(s) >= n) ;
+  void *ps = s;
+
+  while(n--)
+  {
+    *(int*)ps = c;
+    ps++;
+  }
+
+  return s;
 }
 
 void *memmove(void *dst, const void *src, size_t n) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(dst != NULL);
+  assert(src != NULL);
+
+  char *p_dst = dst;
+  const char* p_src = src;
+  size_t i;
+
+  for(i=0; i<n; i++)
+  {
+    *p_dst = *p_src;
+    p_dst++;
+    p_src++;
+  }
+
+  return dst;
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(out != NULL);
+  assert(in != NULL);
+
+  char *p_out = out;
+  const char* p_in = in;
+  assert(p_in + n < p_out);
+  assert(p_out + n < p_in);
+
+  size_t i ;
+
+  for(i=0; i<n; i++)
+  {
+    *p_out = *p_in ;
+    p_out++;
+    p_in++;
+  }
+  return out;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-  panic("Not implemented");
+  // panic("Not implemented");
+  assert(s1 != NULL && s2 != NULL);
+  const char *ps1 = s1;
+  const char *ps2 = s2;
+
+  while(n--)
+  {
+    if(*ps1 == *ps2)
+    {
+      ps1++;
+      ps2++;
+    }
+    else
+    {
+      break;
+    }
+  }
+  
+  return (*ps1 - *ps2);
 }
 
 #endif
