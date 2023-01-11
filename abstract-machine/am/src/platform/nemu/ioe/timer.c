2d00b5b5ae53a35ca50c7bf96a192ee6f6137e2e
lbh <1370656133@qq.com>
2023-01-11 10:40:25 +0800

    Added I/O of timer

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,14 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = 0;
+  uint32_t rtc_time[2] ;
+
+  // pTimeUs = (uint32_t *)uptime->us;
+
+  rtc_time[0] = inl(RTC_ADDR) ;
+  rtc_time[1] = inl(RTC_ADDR + 4) ;
+  uptime->us = (uint64_t)rtc_time[1] << 32 | rtc_time[0] ;
+
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
