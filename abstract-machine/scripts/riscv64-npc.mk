c434aee1d42c98f7c828950c83015791d2597cdc
bli-ambiq <bli@ambiq.com>
2023-07-20 10:21:15 +0800

    Update makefile for arch of ricv64 npc

--- abstract-machine/scripts/riscv64-npc.mk
+++ abstract-machine/scripts/riscv64-npc.mk
@@ -16,11 +16,13 @@ LDFLAGS   += --gc-sections -e _start
 CFLAGS += -DMAINARGS=\"$(mainargs)\"
 .PHONY: $(AM_HOME)/am/src/riscv/npc/trm.c
 
-NEMUFLAGS += 
+NPCFLAGS += 
+
 image: $(IMAGE).elf
 	@$(OBJDUMP) -d $(IMAGE).elf > $(IMAGE).txt
 	@echo + OBJCOPY "->" $(IMAGE_REL).bin
 	@$(OBJCOPY) -S --set-section-flags .bss=alloc,contents -O binary $(IMAGE).elf $(IMAGE).bin
 
 run: image
-	$(MAKE) -C $(NPC_HOME) run RUN_ARGS="$(IMAGE).bin"
+	$(info, image is : $(IMAGE))
+	$(MAKE) -C $(NPC_HOME) run ARGS="$(NPCFLAGS)" IMG=$(IMAGE).bin
